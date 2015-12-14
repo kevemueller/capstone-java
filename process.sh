917bf5546ffd2b0436f9232b2a5fc94c46ebbd05
kevemueller <git@keve.hu>
2015-12-14 02:47:00 +0100

    error handling

--- process.sh
+++ process.sh
@@ -1,7 +1,7 @@
-#for BRANCH in master next; do
-for BRANCH in next; do
-	#for ARCH in default nix32 cross-win32 cross-win64; do
-	for ARCH in cross-win64;  do
+for BRANCH in master next; do
+#for BRANCH in next; do
+	for ARCH in default nix32 cross-win32 cross-win64; do
+	#for ARCH in cross-win64 default;  do
 		echo Processing $BRANCH / $ARCH
 		case $ARCH in
 		cross-win64)
@@ -18,16 +18,19 @@ for BRANCH in next; do
 			;;
 		esac
 
-		pushd capstone
+		pushd capstone > /dev/null
 			git checkout $BRANCH
 			git clean -fx > /dev/null
 			./make.sh $ARCH > ../make-$BRANCH-$ARCH.log 2> ../make-$BRANCH-$ARCH.err
-		popd 
+			E=$?
+		popd > /dev/null
 		rm -rf gen-$BRANCH-$JARCH
-		java -jar lib/jnaerator-0.12-shaded.jar @config.jnaerator @config-$BRANCH.jnaerator -arch $JARCH -o gen-$BRANCH-$JARCH/java @config-$JARCH.jnaerator
-		for i in gen-$BRANCH-$JARCH/java/hu/keve/capstonebinding/*.java; do 
-			sed -i  's/hu.keve.capstonebinding.cs_arm_op.Field1Union/Field1Union/' $i;
-		done
-		javac -cp lib/bridj-0.7.0.jar gen-$BRANCH-$JARCH/java/hu/keve/capstonebinding/*.java
+		if [ $E -eq 0 ]; then 
+			java -jar lib/jnaerator-0.12-shaded.jar @config.jnaerator @config-$BRANCH.jnaerator -arch $JARCH -o gen-$BRANCH-$JARCH/java @config-$JARCH.jnaerator
+			for i in gen-$BRANCH-$JARCH/java/hu/keve/capstonebinding/*.java; do 
+				sed -i  's/hu.keve.capstonebinding.cs_arm_op.Field1Union/Field1Union/' $i;
+			done
+			javac -cp lib/bridj-0.7.0.jar gen-$BRANCH-$JARCH/java/hu/keve/capstonebinding/*.java
+		fi
 	done
 done
